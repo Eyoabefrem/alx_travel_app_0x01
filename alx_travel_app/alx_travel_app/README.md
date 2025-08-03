b62718d71dfa19fa0c9f4586f6ab27b110f31449
Eyoab Tessera <eyoabefrem9@gmail.com>
2025-08-03 20:31:35 +0300

    Milestone 3

---
+++ alx_travel_app/alx_travel_app/README.md
@@ -0,0 +1,13 @@
+## Listings and Bookings API
+
+### Endpoints
+
+- `GET /api/listings/`
+- `POST /api/listings/`
+- `PUT /api/listings/<id>/`
+- `DELETE /api/listings/<id>/`
+
+- `GET /api/bookings/`
+- `POST /api/bookings/`
+- `PUT /api/bookings/<id>/`
+- `DELETE /api/bookings/<id>/`
--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -3,9 +3,11 @@ from .models import Listing, Booking
 from .serializers import ListingSerializer, BookingSerializer
 
 class ListingViewSet(viewsets.ModelViewSet):
+    """Provides CRUD operations for Listings"""
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
+    """Provides CRUD operations for Bookings"""
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
